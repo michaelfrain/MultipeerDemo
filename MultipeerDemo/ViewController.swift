8f4cdaa2c0cd6c519b2339585c6c2a5fe849e088
Michael Frain <michael.frain@hudl.com>
2015-02-18 18:13:48 -0800

    Set up view lifecycle and delegate methods

--- MultipeerDemo/ViewController.swift
+++ MultipeerDemo/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController , MCSessionDelegate, MCBrowserViewControllerDelegate {
+    
+    let serviceType = "GameDataSharing"
+    
+    var peerId: MCPeerID!
+    var session: MCSession!
+    var assistant: MCAdvertiserAssistant!
+    var browser: MCBrowserViewController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.peerId = MCPeerID(displayName: UIDevice.currentDevice().name)
+        self.session = MCSession(peer: self.peerId)
+        self.session.delegate = self
+        
+        self.browser = MCBrowserViewController(serviceType: serviceType, session: self.session)
+        
+        self.browser.delegate = self
+        
+        self.assistant = MCAdvertiserAssistant(serviceType: serviceType, discoveryInfo: nil, session: self.session)
+        
+        self.assistant.start()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showBrowser(sender: UIButton!) {
+        self.presentViewController(self.browser, animated: true, completion: nil)
     }
-
-
+    
+    func session(session: MCSession!, didReceiveData data: NSData!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    func session(session: MCSession!, didStartReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, withProgress progress: NSProgress!) {
+        
+    }
+    
+    func session(session: MCSession!, didFinishReceivingResourceWithName resourceName: String!, fromPeer peerID: MCPeerID!, atURL localURL: NSURL!, withError error: NSError!) {
+        
+    }
+    
+    func session(session: MCSession!, didReceiveStream stream: NSInputStream!, withName streamName: String!, fromPeer peerID: MCPeerID!) {
+        
+    }
+    
+    func session(session: MCSession!, peer peerID: MCPeerID!, didChangeState state: MCSessionState) {
+        
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+        
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        
+    }
+    
+    
 }
 
